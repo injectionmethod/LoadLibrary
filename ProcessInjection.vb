89c8578be57c8555251d0e3943ae3869eec96890
injectionmethod <80434330+injectionmethod@users.noreply.github.com>
2023-05-17 00:02:02 +0100

    Update ProcessInjection.vb

--- ProcessInjection.vb
+++ ProcessInjection.vb
@@ -1,12 +1,11 @@
-﻿
 Module ProcessInjection
-    Private Declare Function VirtualAllocEx Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpAddress As IntPtr, ByVal dwSize As Integer, ByVal flAllocationType As UInteger, ByVal flProtect As UInteger) As IntPtr
-    Private Declare Function WriteProcessMemory Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer As Byte(), ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Boolean
+    Private Declare Function VirtualAllocEx Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpAddress As IntPtr, ByVal dwSize As IntPtr, ByVal flAllocationType As UInteger, ByVal flProtect As UInteger) As IntPtr
+    Private Declare Function WriteProcessMemory Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer As Byte(), ByVal nSize As IntPtr, ByRef lpNumberOfBytesWritten As IntPtr) As Boolean
     Private Declare Function GetProcAddress Lib "kernel32.dll" (ByVal hModule As IntPtr, ByVal lpProcName As String) As IntPtr
-    Private Declare Function GetModuleHandle Lib "kernel32.dll" (ByVal lpModuleName As String) As IntPtr
-    Private Declare Function CreateRemoteThread Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpThreadAttributes As IntPtr, ByVal dwStackSize As Integer, ByVal lpStartAddress As IntPtr, ByVal lpParameter As IntPtr, ByVal dwCreationFlags As UInteger, ByVal lpThreadId As IntPtr) As IntPtr
+    Private Declare Function LoadLibrary Lib "kernel32.dll" Alias "LoadLibraryA" (ByVal lpLibFileName As String) As IntPtr
+    Private Declare Function CreateRemoteThread Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpThreadAttributes As IntPtr, ByVal dwStackSize As IntPtr, ByVal lpStartAddress As IntPtr, ByVal lpParameter As IntPtr, ByVal dwCreationFlags As UInteger, ByVal lpThreadId As IntPtr) As IntPtr
     Private Declare Function WaitForSingleObject Lib "kernel32.dll" (ByVal hHandle As IntPtr, ByVal dwMilliseconds As UInteger) As UInteger
-    Private Declare Function VirtualFreeEx Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpAddress As IntPtr, ByVal dwSize As Integer, ByVal dwFreeType As UInteger) As Boolean
+    Private Declare Function VirtualFreeEx Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpAddress As IntPtr, ByVal dwSize As IntPtr, ByVal dwFreeType As UInteger) As Boolean
     Private Declare Function OpenProcess Lib "kernel32.dll" (ByVal dwDesiredAccess As UInteger, ByVal bInheritHandle As Boolean, ByVal dwProcessId As Integer) As IntPtr
     Private Declare Function CloseHandle Lib "kernel32.dll" (ByVal hObject As IntPtr) As Boolean
 
@@ -26,10 +25,10 @@ Module ProcessInjection
             Dim hProcess As IntPtr = OpenProcess(PROCESS_CREATE_THREAD Or PROCESS_QUERY_INFORMATION Or PROCESS_VM_OPERATION Or PROCESS_VM_WRITE Or PROCESS_VM_READ, False, proc.Id)
 
             Dim dllBytes As Byte() = System.Text.Encoding.ASCII.GetBytes(dllPath)
-            Dim dllPathAlloc As IntPtr = VirtualAllocEx(hProcess, IntPtr.Zero, dllBytes.Length, MEM_COMMIT, PAGE_READWRITE)
-            WriteProcessMemory(hProcess, dllPathAlloc, dllBytes, dllBytes.Length, Nothing)
+            Dim dllPathAlloc As IntPtr = VirtualAllocEx(hProcess, IntPtr.Zero, CType(dllBytes.Length, IntPtr), MEM_COMMIT, PAGE_READWRITE)
+            WriteProcessMemory(hProcess, dllPathAlloc, dllBytes, CType(dllBytes.Length, IntPtr), IntPtr.Zero)
 
-            Dim kernel32 As IntPtr = GetModuleHandle("kernel32.dll")
+            Dim kernel32 As IntPtr = LoadLibrary("kernel32.dll")
             Dim loadLibraryAddr As IntPtr = GetProcAddress(kernel32, "LoadLibraryA")
 
             Dim threadId As IntPtr
@@ -37,11 +36,13 @@ Module ProcessInjection
 
             WaitForSingleObject(hThread, INFINITE)
 
-            VirtualFreeEx(hProcess, dllPathAlloc, dllBytes.Length, &H8000)
+            VirtualFreeEx(hProcess, dllPathAlloc, CType(dllBytes.Length, IntPtr), &H8000)
             CloseHandle(hThread)
             CloseHandle(hProcess)
+
             Return True
         Catch ex As Exception
+            MsgBox(ex.Message)
             Return False
         End Try
     End Function
